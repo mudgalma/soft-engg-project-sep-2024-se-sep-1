e39a9e75c129ad790eb8b63bfa3e433182010e9e
Mahi Mudgal <156433844+mudgalma@users.noreply.github.com>
2026-09-02 16:02:50 +0530

    Refactor app.py for better organization
    Refactor app.py to improve readability and structure.

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask_security import Security, auth_required
 from config import DevelopmentConfig
 from components.extensions import db, bcrypt, cache, datastore
@@ -16,15 +18,19 @@ from components.llm import llm_bp
 from components.github_url import assignment_bp
 from components.admin_stats import admin_dashboard_bp
 
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(DevelopmentConfig)
+
     login_manager = LoginManager()
     login_manager.init_app(app)
+
     app.security = Security(app, datastore)
+
     db.init_app(app)
     bcrypt.init_app(app)
-    
+
     app.register_blueprint(auth_bp)
     app.register_blueprint(project_bp)
     app.register_blueprint(student_submission_bp)
@@ -34,31 +40,31 @@ def create_app():
     app.register_blueprint(llm_bp)
     app.register_blueprint(assignment_bp)
     app.register_blueprint(admin_dashboard_bp)
-    
+
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
-    
-    # CORS for separate servers
-    
-    CORS(app), 
-     
 
-    # Initialize Flask-Caching with Redis
+    CORS(app)
+
     cache.init_app(app)
 
-    # Initialize Redis client
-    #redis_client = Redis(host='localhost', port=6379, db=0)
-    
     return app
 
+
 app = create_app()
+
 
 @app.route('/')
 def hello_world():
     return "Hello World"
 
+
 if __name__ == '__main__':
     print("Running app")
     print(app.url_map)
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
